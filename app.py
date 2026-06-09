5f4c6fbaa0e8d3c045bf9bb84d897f5dd851c1b9
Joyita <mukherjeejoyita11@gmail.com>
2026-06-09 12:46:30 +0530

    submit answer fixed

--- app.py
+++ app.py
@@ -435,8 +435,8 @@ def answer_doubt(did):
     conn = get_db()
     c = conn.cursor()
     answer_files = data.get('answer_files', '')  # JSON string: [{name, data}, ...]
-    c.execute('UPDATE doubts SET answer=?, answer_files=?, status="solved" WHERE id=? AND teacher_id=?',
-              (data['answer'], answer_files, did, uid))
+    c.execute("UPDATE doubts SET answer=?, answer_files=?, status='solved' WHERE id=? AND teacher_id=?",
+              (data.get('answer', ''), answer_files, did, uid))
     conn.commit()
     conn.close()
     return jsonify({'success': True})
